bccfb28344cc1fc6f517cd86adc55aa9019a0430
SN1995dz <SN1995dz@gmail.com>
2022-10-05 22:20:02 +0800

    cache

--- templates/segtree_advanced.cpp
+++ templates/segtree_advanced.cpp
@@ -16,6 +16,17 @@ void build(int rt, int l, int r) {
     PushUp(rt);
 }
 
+void update(int p, int v, int rt, int l, int r) {
+    if (l == r) {
+        sum[rt] += v;
+        return;
+    }
+    int m = (l + r) >> 1;
+    if (p <= m) update(p, v, rt << 1, l, m);
+    else update(p, v, rt << 1 | 1, m + 1, r);
+    PushUp(rt);
+}
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
