c05f2ff8e6add10798a849eae294b64b6e7d3552
SN1995dz <SN1995dz@gmail.com>
2022-10-18 20:51:40 +0800

    optimize KickStart 2022G

--- google/KickStart/2022G/2.cpp
+++ google/KickStart/2022G/2.cpp
@@ -2,7 +2,35 @@
 
 using namespace std;
 
-const int INF = 1e9;
+template<typename T> class sparse_table {
+    public:
+        int n;
+        vector<vector<T>> f;
+
+        inline int LOG2(unsigned int x) {
+            return 31 - __builtin_clz(x);
+        }
+
+        sparse_table(const vector<T> &v) {
+            n = v.size();
+            assert(n > 0);
+            f.resize(n, vector<T>(LOG2(n) + 1));
+            for (int i = 0; i < n; ++i) f[i][0] = v[i];
+            for (int j = 1; (1 << j) <= n; ++j) {
+                for (int i = 0; i + (1 << j) - 1 < n; ++i) {
+                    //min or max
+                    f[i][j] = min(f[i][j - 1], f[i + (1 << (j - 1))][j - 1]);
+                }
+            }
+        }
+
+        T get(int l, int r) {
+            assert(0 <= l && l <= r && r < n);
+            int k = LOG2(r - l + 1);
+            //min or max
+            return min(f[l][k], f[r - (1 << k) + 1][k]);
+        }
+};
 
 void solve() {
     int n;
@@ -19,23 +47,13 @@ void solve() {
     for (int i = 0; i <= n; ++i) {
         b[i + 1] = b[i] + s[i];
     }
-    vector<vector<int>> f(n + 1, vector<int>(20));
-    for (int i = 1; i <= n; ++i) f[i][0] = s[i];
-    for (int j = 1; (1 << j) <= n; ++j) {
-        for (int i = 1; i + (1 << j) - 1 <= n; ++i) {
-            f[i][j] = min(f[i][j - 1], f[i + (1 << (j - 1))][j - 1]);
-        }
-    }
-    auto rmq = [&](int l, int r) {
-        int k = log2(r - l + 1);
-        return min(f[l][k], f[r - (1 << k) + 1][k]);
-    };
+    sparse_table<int> st(s);
     long long ans = 0;
     for (int i = 1; i <= n; ++i) {
         int l = i, r = n, res = -1;
         while (l <= r) {
             int mid = (l + r) / 2;
-            if (rmq(i, mid) - s[i - 1] >= 0) {
+            if (st.get(i, mid) - s[i - 1] >= 0) {
                 res = mid;
                 l = mid + 1;
             } else {
