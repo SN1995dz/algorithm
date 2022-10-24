fdb8642eddcb094508200eb887c3c160d30a7c77
SN1995dz <SN1995dz@gmail.com>
2022-10-24 22:37:37 +0800

    pbds

--- templates/pbds.cpp
+++ templates/pbds.cpp
@@ -1,7 +1,5 @@
 #include <bits/stdc++.h>
-//#include <ext/pb_ds/tree_policy.hpp>
 #include <ext/pb_ds/assoc_container.hpp>
-//#include <ext/pb_ds/detail/standard_policies.hpp>
  
 using namespace std;
 using namespace __gnu_pbds;
