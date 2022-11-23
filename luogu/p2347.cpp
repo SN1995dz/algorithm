ad39b28a052233db7920bdb7af1f6d2df19a4e5a
SN1995dz <SN1995dz@gmail.com>
2022-11-23 22:12:16 +0800

    knapsack

--- luogu/p2347.cpp
+++ luogu/p2347.cpp
@@ -8,18 +8,21 @@ int main() {
     cout.tie(0);
     vector<int> a(6);
     const int w[6] = {1, 2, 3, 5, 10, 20};
-    vector<bool> dp(1001, false);
-    dp[0] = true;
     for (int i = 0; i < 6; ++i) cin >> a[i];
+    vector<vector<int>> f(7, vector<int>(1001, -1));
+    f[0][0] = 0;
     for (int i = 0; i < 6; ++i) {
-        for (int j = 0; j < a[i]; ++j) {
-            for (int k = 1000; k >= w[i]; --k) {
-                if (dp[k - w[i]] == true) dp[k] = true;
+        for (int j = 0; j <= 1000; ++j) {
+            if (f[i][j] >= 0) f[i + 1][j] = a[i];
+        }
+        for (int j = 0; j + w[i] <= 1000; ++j) {
+            if (f[i + 1][j] > 0) {
+                f[i + 1][j + w[i]] = max(f[i + 1][j + w[i]], f[i + 1][j] - 1);
             }
         }
     }
     int ans = 0;
-    for (int i = 1; i <= 1000; ++i) ans += (dp[i] == true);
+    for (int i = 1; i <= 1000; ++i) ans += (f[6][i] >= 0);
     cout << "Total=" << ans << endl;
     return 0;
 }
