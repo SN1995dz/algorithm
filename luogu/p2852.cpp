423cc8ef58853b2623c239def1b692a4130a8d20
SN1995dz <SN1995dz@gmail.com>
2024-09-18 11:02:15 +0800

    feat: add SuffixArray

---
+++ luogu/p2852.cpp
@@ -0,0 +1,114 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template <typename T>
+vector<int> getSA(int n, const T &s, int char_bound=256) {
+    vector<int> a(n);
+    if (n == 0) return a;
+    if (char_bound != -1) {
+        vector<int> aux(char_bound, 0);
+        for (int i = 0; i < n; ++i) ++aux[s[i]];
+        int sum = 0;
+        for (int i = 0; i < char_bound; ++i) {
+            int add = aux[i];
+            aux[i] = sum;
+            sum += add;
+        }
+        for (int i = 0; i < n; ++i) a[aux[s[i]]++] = i;
+    } else {
+        iota(a.begin(), a.end(), 0);
+        sort(a.begin(), a.end(), [&s](int i, int j) { return s[i] < s[j]; });
+    }
+    vector<int> sorted_by_second(n);
+    vector<int> ptr_group(n);
+    vector<int> new_group(n);
+    vector<int> group(n);
+    group[a[0]] = 0;
+    for (int i = 1; i < n; ++i) {
+        group[a[i]] = group[a[i - 1]] + (!(s[a[i]] == s[a[i - 1]]));
+    }
+    int cnt = group[a[n - 1]] + 1;
+    int step = 1;
+    while (cnt < n) {
+        int at = 0;
+        for (int i = n - step; i < n; ++i) sorted_by_second[at++] = i;
+        for (int i = 0; i < n; ++i) {
+            if (a[i] - step >= 0) {
+                sorted_by_second[at++] = a[i] - step;
+            }
+        }
+        for (int i = n - 1; i >= 0; --i) ptr_group[group[a[i]]] = i;
+        for (int i = 0; i < n; ++i) {
+            int x = sorted_by_second[i];
+            a[ptr_group[group[x]]++] = x;
+        }
+        new_group[a[0]] = 0;
+        for (int i = 1; i < n; ++i) {
+            if (group[a[i]] != group[a[i - 1]]) {
+                new_group[a[i]] = new_group[a[i - 1]] + 1;
+            } else {
+                int pre = (a[i - 1] + step >= n ? -1 : group[a[i - 1] + step]);
+                int cur = (a[i] + step >= n ? -1 : group[a[i] + step]);
+                new_group[a[i]] = new_group[a[i - 1]] + (pre != cur);
+            }
+        }
+        swap(group, new_group);
+        cnt = group[a[n - 1]] + 1;
+        step <<= 1;
+    }
+    return a;
+}
+
+template <typename T>
+vector<int> getHeight(int n, const T &s, const vector<int> &sa) {
+    assert((int)sa.size() == n);
+    vector<int> rk(n);
+    for (int i = 0; i < n; ++i) rk[sa[i]] = i;
+
+    vector<int> Height(n, 0);
+    for (int i = 0, k = 0; i < n; ++i) {
+        if (k) --k;
+        if (rk[i] == n - 1) k = 0;
+        else {
+            int j = sa[rk[i] + 1];
+            while (i + k < n && j + k < n && s[i + k] == s[j + k]) ++k;
+            Height[rk[i] + 1] = k;
+        }
+    }
+    return Height;
+}
+
+template <typename T>
+vector<int> getSA(const T &s, int char_bound=256) {
+    return getSA((int)s.size(), s, char_bound);
+}
+
+template <typename T>
+vector<int> getHeight(const T &s, const vector<int> &sa) {
+    return getHeight((int)s.size(), s, sa);
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n, k;
+    cin >> n >> k;
+    vector<int> q(n);
+    for (int i = 0; i < n; ++i) cin >> q[i];
+    vector<int> sa = getSA(q, -1);
+    vector<int> H = getHeight(q, sa);
+    deque<pair<int, int>> Q;
+    int ans = 0;
+    for (int i = 1; i < n; ++i) {
+        while (!Q.empty() && Q.back().first >= H[i]) Q.pop_back();
+        Q.push_back(make_pair(H[i], i));
+        if (i >= k - 1) {
+            while (Q.front().second <= i - (k - 1)) Q.pop_front();
+            ans = max(ans, Q.front().first);
+        }
+    }
+    cout << ans << endl;
+    return 0;
+}
+
---
+++ luogu/p2870.cpp
@@ -0,0 +1,100 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template <typename T>
+vector<int> getSA(int n, const T &s, int char_bound=256) {
+    vector<int> a(n);
+    if (n == 0) return a;
+    if (char_bound != -1) {
+        vector<int> aux(char_bound, 0);
+        for (int i = 0; i < n; ++i) ++aux[s[i]];
+        int sum = 0;
+        for (int i = 0; i < char_bound; ++i) {
+            int add = aux[i];
+            aux[i] = sum;
+            sum += add;
+        }
+        for (int i = 0; i < n; ++i) a[aux[s[i]]++] = i;
+    } else {
+        iota(a.begin(), a.end(), 0);
+        sort(a.begin(), a.end(), [&s](int i, int j) { return s[i] < s[j]; });
+    }
+    vector<int> sorted_by_second(n);
+    vector<int> ptr_group(n);
+    vector<int> new_group(n);
+    vector<int> group(n);
+    group[a[0]] = 0;
+    for (int i = 1; i < n; ++i) {
+        group[a[i]] = group[a[i - 1]] + (!(s[a[i]] == s[a[i - 1]]));
+    }
+    int cnt = group[a[n - 1]] + 1;
+    int step = 1;
+    while (cnt < n) {
+        int at = 0;
+        for (int i = n - step; i < n; ++i) sorted_by_second[at++] = i;
+        for (int i = 0; i < n; ++i) {
+            if (a[i] - step >= 0) {
+                sorted_by_second[at++] = a[i] - step;
+            }
+        }
+        for (int i = n - 1; i >= 0; --i) ptr_group[group[a[i]]] = i;
+        for (int i = 0; i < n; ++i) {
+            int x = sorted_by_second[i];
+            a[ptr_group[group[x]]++] = x;
+        }
+        new_group[a[0]] = 0;
+        for (int i = 1; i < n; ++i) {
+            if (group[a[i]] != group[a[i - 1]]) {
+                new_group[a[i]] = new_group[a[i - 1]] + 1;
+            } else {
+                int pre = (a[i - 1] + step >= n ? -1 : group[a[i - 1] + step]);
+                int cur = (a[i] + step >= n ? -1 : group[a[i] + step]);
+                new_group[a[i]] = new_group[a[i - 1]] + (pre != cur);
+            }
+        }
+        swap(group, new_group);
+        cnt = group[a[n - 1]] + 1;
+        step <<= 1;
+    }
+    return a;
+}
+
+template <typename T>
+vector<int> getSA(const T &s, int char_bound=256) {
+    return getSA((int)s.size(), s, char_bound);
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n;
+    cin >> n;
+    string s;
+    for (int i = 0; i < n; ++i) {
+        string tmp;
+        cin >> tmp;
+        s.push_back(tmp[0]);
+    }
+    for (int i = n - 1; i >= 0; --i) s.push_back(s[i]);
+    vector<int> sa = getSA(s);
+    vector<int> rk(n + n);
+    for (int i = 0; i < n + n; ++i) rk[sa[i]] = i;
+    string ans;
+    int l = 0, r = n - 1;
+    while (l <= r) {
+        if (rk[l] < rk[n + n - r - 1]) {
+            ans.push_back(s[l]);
+            ++l;
+        } else {
+            ans.push_back(s[r]);
+            --r;
+        }
+    }
+    for (int i = 0; i < n; ++i) {
+        cout << ans[i];
+        if ((i + 1) % 80 == 0) cout << endl;
+    }
+    return 0;
+}
+
